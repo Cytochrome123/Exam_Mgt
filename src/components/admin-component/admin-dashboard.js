99cd52aafff0047b55f18fdf148893926542d0ab
Hudhayfah Ismail <hoismail2017@gmail.com>
2022-11-15 16:41:44 +0100

    fixed redundant

--- src/components/admin-component/admin-dashboard.js
+++ src/components/admin-component/admin-dashboard.js
@@ -2,22 +2,23 @@ import { Component } from 'react';
 import axios from 'axios';
 
 
-import Grid from '@mui/material/Grid';
+// import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+// import Box from '@mui/material/Box';
+// import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+
 import Navbarr from '../header/navbar';
 
 
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-  height: 60,
-  lineHeight: '60px',
-  width: '20%',
-  // display: 'flex'
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//   ...theme.typography.body2,
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+//   height: 60,
+//   lineHeight: '60px',
+//   width: '20%',
+//   // display: 'flex'
+// }));
 
 // const darkTheme = createTheme({ palette: { mode: 'dark' } });
 // const lightTheme = createTheme({ palette: { mode: 'light' } });
--- src/components/examiner-component/exam-component/createExam.js
+++ src/components/examiner-component/exam-component/createExam.js
@@ -1,5 +1,5 @@
 import { useEffect, useContext, useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import { Alert } from 'react-bootstrap';
 
 
